a982a3b1ba63db9562e026f3d34080ad631870a2
filip patrman <filip.patrman.023@pslib.cz>
2025-02-19 16:31:58 +0100

    megaaa

---
+++ script/script.js
@@ -0,0 +1,8 @@
+//close button for modal
+    document.querySelector('.modal--AddPR__close').addEventListener('click', function() {
+        document.querySelector('.modal--AddPR').style.display = 'none';
+    });
+
+    document.querySelector('.modal--AddPR__open').addEventListener('click', function() {
+        document.querySelector('.modal--AddPR').style.display = 'flex';
+    });
